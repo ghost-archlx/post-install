f1d1a7ba930d4fee11daa61bd3f91bf6dd952625
ghost <46002572+ghost-archlx@users.noreply.github.com>
2023-08-26 03:48:06 -0300

    Create flatpak.sh

---
+++ flatpak.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# -*- ENCODING: UTF-8 -*-
+
+flatpak install flathub io.github.aandrew_me.ytdn && flatpak install flathub com.github.micahflee.torbrowser-launcher &&
+flatpak install flathub com.ktechpit.whatsie && flatpak install flathub com.github.neithern.g4music
