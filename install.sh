ef0a7f04b5e6ce27bf91a85bd7e24dd510709c2d
ghost <46002572+ghost-archlx@users.noreply.github.com>
2023-08-26 03:37:54 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# -*- ENCODING: UTF-8 -*-
+
+sudo pacman -S nvidia-lts nvidia-settings nvidia-utils lib32-nvidia-utils nvidia-prime nvtop linux-lts-headers 
+audacity audacious ffmpegthumbnailer totem vlc evince gvfs-mtp shotcut flatpak eog eog-plugins telegram-desktop 
+gnome-browser-connector gnome-disk-utility gnome-calculator gnome-clocks simple-scan gnome-tweaks gstreamer 
+gst-libav gst-plugins-bad gst-plugins-ugly gst-pluins-good gst-plugins-base 
+
+
+##sudo chmod a+x install.sh##
