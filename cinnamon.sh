6a2acd2e312f61b2b22ed056c74c38455454f103
ghost <46002572+ghost-archlx@users.noreply.github.com>
2024-10-21 09:36:19 -0300

    Update cinnamon.sh

--- cinnamon.sh
+++ cinnamon.sh
@@ -3,4 +3,5 @@
 # -*- ENCODING: UTF-8 -*-
 
 sudo pacman -S nvidia-lts nvidia-settings lib32-nvidia-utils nvtop gnome-system-monitor gnome-disk-utility vim totem vlc gvfs-mtp 
-neofetch os-prober qbittorrent inux-lts-headers audacity audacious
+neofetch os-prober qbittorrent inux-lts-headers audacity audacious gstreamer gst-libav gst-plugins-bad gst-plugins-ugly 
+gst-pluins-good gst-plugins-base xed xreader 
