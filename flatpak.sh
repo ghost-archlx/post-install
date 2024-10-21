0e8eabcaf3a53f2b9c6fbea732662b440634de96
ghost <46002572+ghost-archlx@users.noreply.github.com>
2024-10-21 09:32:27 -0300

    Update flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -2,5 +2,9 @@
 
 # -*- ENCODING: UTF-8 -*-
 
-flatpak install flathub io.github.aandrew_me.ytdn && flatpak install flathub com.github.micahflee.torbrowser-launcher &&
-flatpak install flathub com.ktechpit.whatsie && flatpak install flathub com.github.neithern.g4music
+flatpak install flathub com.discordapp.Discord && flatpak install flathub com.getmailspring.Mailspring &&
+flatpak install flathub com.google.Chrome && flatpak install flathub com.rtosta.zapzap && 
+flatpak install flathub com.simplenote.Simplenote && flatpak install flathub com.spotify.Client &&
+flatpak install flathub com.todoist.Todoist && flatpak install flathub io.github.zen_browser.zen &&
+flatpak install flathub org.feichtmeier.Musicpod && flatpak install flathub org.mozilla.firefox &&
+flatpak install flathub org.telegram.desktop
